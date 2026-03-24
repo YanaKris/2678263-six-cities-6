b01137d4823c0897f3e20a29311c31a6e41c4da1
Yana <holoimenko.yana@yandex.ru>
2026-03-24 20:53:00 +0700

    5.1.5 Правки

--- src/hooks/use-map.tsx
+++ src/hooks/use-map.tsx
@@ -1,50 +1,40 @@
-import { useEffect, useState, MutableRefObject, useRef } from 'react';
-import { Map, TileLayer } from 'leaflet';
+import { useEffect, useRef } from 'react';
+import { map as leafletMap, tileLayer, Map as LeafletMap } from 'leaflet';
 import { City } from '../type/city';
 
-export default function useMap(
-  mapRef: MutableRefObject<HTMLElement | null>,
-  city: City,
-): Map | null {
-  const [map, setMap] = useState<Map | null>(null);
-  const isRenderedRef = useRef<boolean>(false);
+export default function useMap(mapRef: React.RefObject<HTMLDivElement>, city: City) {
+  const mapRefInstance = useRef<LeafletMap | null>(null);
 
   useEffect(() => {
-    if (mapRef.current !== null && !isRenderedRef.current) {
-      const instance = new Map(mapRef.current, {
-        center: {
-          lat: city.location.latitude,
-          lng: city.location.longitude,
-        },
-        zoom: city.location.zoom,
-      });
-
-      const layer = new TileLayer(
-        'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
-        {
-          attribution:
-            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-        },
-      );
-
-      instance.addLayer(layer);
-
-      setMap(instance);
-      isRenderedRef.current = true;
+    if (!mapRef.current || mapRefInstance.current) {
+      return;
     }
-  }, [mapRef, city]);
+
+    const instance = leafletMap(mapRef.current, {
+      center: [city.location.latitude, city.location.longitude],
+      zoom: city.location.zoom,
+    });
+
+    tileLayer(
+      'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+      {
+        attribution: '&copy; OpenStreetMap contributors &copy; CARTO',
+      },
+    ).addTo(instance);
+
+    mapRefInstance.current = instance;
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   useEffect(() => {
-    if (map) {
-      map.setView(
-        {
-          lat: city.location.latitude,
-          lng: city.location.longitude,
-        },
+    if (mapRefInstance.current) {
+      mapRefInstance.current.setView(
+        [city.location.latitude, city.location.longitude],
         city.location.zoom,
       );
     }
-  }, [map, city]);
+  }, [city.location.latitude, city.location.longitude, city.location.zoom]);
 
-  return map;
+  return mapRefInstance.current;
 }
