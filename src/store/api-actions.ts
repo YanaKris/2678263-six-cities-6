f5bb7d4b7aa15def4835ab5710aac277d554d2a4
Yana <holoimenko.yana@yandex.ru>
2026-04-05 14:54:06 +0700

    7.1.3 Правки. Стаил код

--- src/store/api-actions.ts
+++ src/store/api-actions.ts
@@ -12,32 +12,37 @@ export const fetchOffersAction = createAsyncThunk<
   void,
   undefined,
   { extra: AxiosInstance }
->('offers/fetchOffers', async (_arg, { dispatch, extra: api }) => {
-  dispatch(setOffersLoading(true));
+>(
+  'offers/fetchOffers',
+  async (_arg, { dispatch, extra: api }) => {
+    dispatch(setOffersLoading(true));
 
-  try {
-    const { data } = await api.get<Offer[]>('/offers');
-    dispatch(setOffers(data));
-  } catch (e) {
-  } finally {
-    dispatch(setOffersLoading(false));
+    try {
+      const { data } = await api.get<Offer[]>('/offers');
+      dispatch(setOffers(data));
+    } finally {
+      dispatch(setOffersLoading(false));
+    }
   }
-});
+);
 
 export const fetchOfferByIdAction = createAsyncThunk<
   void,
   string,
   { extra: AxiosInstance }
->('offer/fetchById', async (offerId, { dispatch, extra: api }) => {
-  dispatch(setOfferLoading(true));
-  dispatch(setCurrentOffer(null));
-
-  try {
-    const { data } = await api.get<Offer>(`/offers/${offerId}`);
-    dispatch(setCurrentOffer(data));
-  } catch (e) {
+>(
+  'offer/fetchById',
+  async (offerId, { dispatch, extra: api }) => {
+    dispatch(setOfferLoading(true));
     dispatch(setCurrentOffer(null));
-  } finally {
-    dispatch(setOfferLoading(false));
+
+    try {
+      const { data } = await api.get<Offer>(`/offers/${offerId}`);
+      dispatch(setCurrentOffer(data));
+    } catch (e) {
+      dispatch(setCurrentOffer(null));
+    } finally {
+      dispatch(setOfferLoading(false));
+    }
   }
-});
+);
